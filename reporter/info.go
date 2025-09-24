4e755e0b787103f485f8376e3b54bbc9be68afa4
andynesse <andynesse04@gmail.com>
2025-09-24 11:39:06 +0200

    style: Add emojis to the usage print

--- reporter/info.go
+++ reporter/info.go
@@ -23,26 +23,26 @@ func getModuleVersion() string {
 }
 
 func PrintUsage() {
-	usageOutput := "commit-analyzer - Analyze your git commit messages for quality\n\n"
-	usageOutput += "Usage:\n"
-	usageOutput += "  commit-analyzer [flags]\n\n"
-	usageOutput += "Flags:\n"
+	usageOutput := "commit-analyzer - Analyze the quality of your git commit messages\n\n"
+	usageOutput += "📚 Usage:\n"
+	usageOutput += "   commit-analyzer [flags]\n\n"
+	usageOutput += "🚩 Flags:\n"
 
 	var b bytes.Buffer
 	w := tabwriter.NewWriter(&b, 0, 0, 4, ' ', 0)
-	fmt.Fprintln(w, "  -repo string\tPath to git repository (default \".\")")
-	fmt.Fprintln(w, "  -since string\tAnalyze commits since")
-	fmt.Fprintln(w, "  -until string\tAnalyze commits until")
-	fmt.Fprintln(w, "  -limit int\tLimit number of commits to analyze (0 = all)")
-	fmt.Fprintln(w, "  -version\tShow module version")
-	fmt.Fprintln(w, "  -help\tShow this help message")
+	fmt.Fprintln(w, "   -repo string\tPath to git repository (default \".\")")
+	fmt.Fprintln(w, "   -since string\tAnalyze commits since")
+	fmt.Fprintln(w, "   -until string\tAnalyze commits until")
+	fmt.Fprintln(w, "   -limit int\tLimit number of commits to analyze (0 = all)")
+	fmt.Fprintln(w, "   -version\tShow module version")
+	fmt.Fprintln(w, "   -help\tShow this help message")
 	w.Flush()
 	usageOutput += b.String()
 
-	usageOutput += "\nExamples:\n"
-	usageOutput += "  commit-analyzer -repo ./path/to/my-repo\n"
-	usageOutput += "  commit-analyzer -since \"2025-01-01\" -until \"2025-02-01\"\n"
-	usageOutput += "  commit-analyzer -limit 5\n\n"
+	usageOutput += "\n🛠  Examples:\n"
+	usageOutput += "   commit-analyzer -repo ./path/to/my-repo\n"
+	usageOutput += "   commit-analyzer -since \"2025-01-01\" -until \"2025-02-01\"\n"
+	usageOutput += "   commit-analyzer -limit 5\n\n"
 	usageOutput += "For more information, visit: https://github.com/andynesse/commit-analyzer\n"
 	fmt.Println(usageOutput)
 }
